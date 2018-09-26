cf617b11a2862a6e5efe1765a0e9e2803964d008
Stefan Heidekrüger <stefan.heidekrueger@tum.de>
2018-09-26 17:54:06 +0200

    changed from python unittesting to pytest

--- bnelearn/tests/test_dummy.py
+++ bnelearn/tests/test_dummy.py
@@ -1,8 +1,5 @@
-from unittest import TestCase
-
 import bnelearn
 
-class TestDummy(TestCase):
-    def test_is_string(self):
-        s=bnelearn.joke()
-        self.assertTrue(isinstance(s, str))
+def test_is_string():
+    s=bnelearn.joke()
+    assert isinstance(s, str)
