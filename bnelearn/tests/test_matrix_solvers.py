f0e3c3d6ec569433158712b0eb03b76ce50557e6
Paul Sutterer <paul.sutterer@tum.de>
2019-05-27 15:28:35 +0200

    Stefan hats kaputt gemacht...Merge endlich!

--- bnelearn/tests/test_matrix_solvers.py
+++ bnelearn/tests/test_matrix_solvers.py
@@ -28,7 +28,7 @@ def run_fictitious_play(n_players, game, initial_beliefs=None, iterations=1000):
                 batch_size=batch_size,
                 strategy_to_bidder_closure=strat_to_player)
     
-    return env.strategyve_with_fictitious_play(
+    return env.solve_with_fictitious_play(
         dev = device,
         initial_beliefs=initial_beliefs,
         iterations = iterations)
@@ -42,7 +42,7 @@ def run_smooth_fictitious_play(n_players, game, initial_beliefs=None, iterations
                 batch_size=batch_size,
                 strategy_to_bidder_closure=strat_to_player)
     
-    return env.strategyve_with_smooth_fictitious_play(
+    return env.solve_with_smooth_fictitious_play(
         dev = device,
         initial_beliefs=initial_beliefs,
         iterations = iterations)
@@ -56,7 +56,7 @@ def run_k_smooth_fictitious_play(n_players, game, initial_beliefs=None, iteratio
                 batch_size=batch_size,
                 strategy_to_bidder_closure=strat_to_player)
     
-    return env.strategyve_with_k_smooth_fictitious_play(
+    return env.solve_with_k_smooth_fictitious_play(
         dev = device,
         initial_beliefs=initial_beliefs,
         iterations = iterations)
@@ -65,7 +65,7 @@ def run_k_smooth_fictitious_play(n_players, game, initial_beliefs=None, iteratio
 # Test Fictitious Play
 
 def test_fictitious_play_with_PrisonersDilemmy_2x2():    
-    strategy = run_fictitious_play(2, PrisonersDilemma(device = device),
+    strategy = run_fictitious_play(2, PrisonersDilemma(cuda = cuda),
                               iterations = 100)[2][:]
                                
     for i in range(len(strategy)-1):
@@ -73,7 +73,7 @@ def test_fictitious_play_with_PrisonersDilemmy_2x2():
                           torch.tensor([0,1],dtype = torch.float,device = device),atol = 0.001)
  
 def test_fictitious_play_with_MatchingPennies_2x2():
-    strategy = run_fictitious_play(2, MatchingPennies(device = device),
+    strategy = run_fictitious_play(2, MatchingPennies(cuda = cuda),
                               iterations = 100)[2][:]
                                
     for i in range(len(strategy)-1):
@@ -81,7 +81,7 @@ def test_fictitious_play_with_MatchingPennies_2x2():
                           torch.tensor([0.5,0.5],dtype = torch.float, device = device),atol = 0.01) 
      
 def test_fictitious_play_with_BattleOfTheSexes_2x2():
-    strategy = run_fictitious_play(2, BattleOfTheSexes(device = device),
+    strategy = run_fictitious_play(2, BattleOfTheSexes(cuda = cuda),
                               initial_beliefs = [torch.tensor([1,0], dtype = torch.float, device = device), torch.tensor([0,1], dtype = torch.float, device = device)],
                               iterations = 100)[2][:]
                                                
@@ -89,7 +89,7 @@ def test_fictitious_play_with_BattleOfTheSexes_2x2():
         assert torch.allclose(strategy[i],                                       
                            torch.tensor([0,1], dtype = torch.float, device = device),atol = 0.01)
       
-    strategy = run_fictitious_play(2, BattleOfTheSexes(device = device),
+    strategy = run_fictitious_play(2, BattleOfTheSexes(cuda = cuda),
                               initial_beliefs = [torch.tensor([1,0], dtype = torch.float, device = device), torch.tensor([1,0], dtype = torch.float, device = device)],
                               iterations = 100)[2][:], 
      
@@ -98,14 +98,14 @@ def test_fictitious_play_with_BattleOfTheSexes_2x2():
                            torch.tensor([1,0], dtype = torch.float, device = device),atol = 0.01)
          
 def test_fictitious_play_with_BattleOfTheSexes_Mod_3x2():
-    strategy = run_fictitious_play(2, BattleOfTheSexes_Mod(device = device),
+    strategy = run_fictitious_play(2, BattleOfTheSexes_Mod(cuda = cuda),
                               iterations = 100)[2][:]
                                                
     assert torch.allclose(strategy[0], torch.tensor([1,0,0], dtype = torch.float, device = device),atol = 0.01)
     assert torch.allclose(strategy[1], torch.tensor([1,0], dtype = torch.float, device = device),atol = 0.01)
              
 def test_fictitious_play_with_PaulTestGame_2x2x2():
-    strategy = run_fictitious_play(3, PaulTestGame(device = device),
+    strategy = run_fictitious_play(3, PaulTestGame(cuda = cuda),
                               initial_beliefs = [torch.tensor([0,1], dtype = torch.float, device = device),
                                                  torch.tensor([0,1], dtype = torch.float, device = device),
                                                  torch.tensor([0,1], dtype = torch.float, device = device)],
@@ -118,7 +118,7 @@ def test_fictitious_play_with_PaulTestGame_2x2x2():
 # Test smooth Fictitious Play
  
 def test_smooth_fictitious_play_with_PrisonersDilemmy_2x2():    
-    strategy = run_smooth_fictitious_play(2, PrisonersDilemma(device = device),
+    strategy = run_smooth_fictitious_play(2, PrisonersDilemma(cuda = cuda),
                               iterations = 1000)[2][:]
                                 
     for i in range(len(strategy)-1):
@@ -126,7 +126,7 @@ def test_smooth_fictitious_play_with_PrisonersDilemmy_2x2():
                           torch.tensor([0,1],dtype = torch.float,device = device),atol = 0.001)
    
 def test_smooth_fictitious_play_with_MatchingPennies_2x2():
-    strategy = run_smooth_fictitious_play(2, MatchingPennies(device = device),
+    strategy = run_smooth_fictitious_play(2, MatchingPennies(cuda = cuda),
                               iterations = 1000)[2][:]
                                 
     for i in range(len(strategy)-1):
@@ -135,7 +135,7 @@ def test_smooth_fictitious_play_with_MatchingPennies_2x2():
    
 def test_smooth_fictitious_play_with_BattleOfTheSexes_2x2():
     # -> All initial beliefs which are mirrored lead to mixed Nash
-    strategy = run_smooth_fictitious_play(2, BattleOfTheSexes(device = device),
+    strategy = run_smooth_fictitious_play(2, BattleOfTheSexes(cuda = cuda),
                                      initial_beliefs = [torch.tensor([0.5,0.5], dtype = torch.float, device = device),
                                                         torch.tensor([0.5,0.5], dtype = torch.float, device = device)],
                                      iterations = 3000)[2][:]
@@ -144,7 +144,7 @@ def test_smooth_fictitious_play_with_BattleOfTheSexes_2x2():
     assert torch.allclose(strategy[1], torch.tensor([0.4,0.6], dtype = torch.float, device = device),atol = 0.01)                                                
         
     # Test pure Nash
-    strategy = run_smooth_fictitious_play(2, BattleOfTheSexes(device = device),
+    strategy = run_smooth_fictitious_play(2, BattleOfTheSexes(cuda = cuda),
                                      initial_beliefs = [torch.tensor([0.8,0.2], dtype = torch.float, device = device),
                                                         torch.tensor([0.8,0.2], dtype = torch.float, device = device)],
                                      iterations = 3000)[2][:]
@@ -153,7 +153,7 @@ def test_smooth_fictitious_play_with_BattleOfTheSexes_2x2():
                           torch.tensor([1,0],dtype = torch.float, device = device),atol = 0.01)                                                  
  
 def test_smooth_fictitious_play_with_PaulTestGame_2x2x2():
-    expected_valuation = run_smooth_fictitious_play(3, PaulTestGame(device = device),
+    expected_valuation = run_smooth_fictitious_play(3, PaulTestGame(cuda = cuda),
                                      initial_beliefs = [torch.tensor([0.1,0.9], dtype = torch.float, device = device),
                                                         torch.tensor([0.2,0.8], dtype = torch.float, device = device),
                                                         torch.tensor([0.3,0.7], dtype = torch.float, device = device)],
@@ -165,7 +165,7 @@ def test_smooth_fictitious_play_with_PaulTestGame_2x2x2():
  
  
  
-    strategy = run_fictitious_play(3, PaulTestGame(device = device),
+    strategy = run_fictitious_play(3, PaulTestGame(cuda = cuda),
                               initial_beliefs = [torch.tensor([0,1], dtype = torch.float, device = device),
                                                  torch.tensor([0,1], dtype = torch.float, device = device),
                                                  torch.tensor([0,1], dtype = torch.float, device = device)],
@@ -178,7 +178,7 @@ def test_smooth_fictitious_play_with_PaulTestGame_2x2x2():
    # Test k Smooth Fictitious Play
 
 def test_k_smooth_fictitious_play_with_PrisonersDilemmy_2x2():    
-    strategy = run_k_smooth_fictitious_play(2, PrisonersDilemma(device = device),
+    strategy = run_k_smooth_fictitious_play(2, PrisonersDilemma(cuda = cuda),
                               iterations = 1000)[2][:]
                                
     for i in range(len(strategy)-1):
@@ -186,7 +186,7 @@ def test_k_smooth_fictitious_play_with_PrisonersDilemmy_2x2():
                           torch.tensor([0,1],dtype = torch.float,device = device),atol = 0.001)
   
 def test_k_smooth_fictitious_play_with_MatchingPennies_2x2():
-    strategy = run_k_smooth_fictitious_play(2, MatchingPennies(device = device),
+    strategy = run_k_smooth_fictitious_play(2, MatchingPennies(cuda = cuda),
                               iterations = 1000)[2][:]
                                
     for i in range(len(strategy)-1):
@@ -195,7 +195,7 @@ def test_k_smooth_fictitious_play_with_MatchingPennies_2x2():
   
 def test_k_smooth_fictitious_play_with_BattleOfTheSexes_2x2():
     # -> All initial beliefs which are mirrored lead to mixed Nash
-    strategy = run_k_smooth_fictitious_play(2, BattleOfTheSexes(device = device),
+    strategy = run_k_smooth_fictitious_play(2, BattleOfTheSexes(cuda = cuda),
                                      initial_beliefs = [torch.tensor([0.5,0.5], dtype = torch.float, device = device),
                                                         torch.tensor([0.5,0.5], dtype = torch.float, device = device)],
                                      iterations = 3000)[2][:]
@@ -204,7 +204,7 @@ def test_k_smooth_fictitious_play_with_BattleOfTheSexes_2x2():
     assert torch.allclose(strategy[1], torch.tensor([0.4,0.6], dtype = torch.float, device = device),atol = 0.01)                                                
        
     # Test pure Nash
-    strategy = run_k_smooth_fictitious_play(2, BattleOfTheSexes(device = device),
+    strategy = run_k_smooth_fictitious_play(2, BattleOfTheSexes(cuda = cuda),
                                      initial_beliefs = [torch.tensor([0.8,0.2], dtype = torch.float, device = device),
                                                         torch.tensor([0.8,0.2], dtype = torch.float, device = device)],
                                      iterations = 3000)[2][:]
@@ -213,7 +213,7 @@ def test_k_smooth_fictitious_play_with_BattleOfTheSexes_2x2():
                           torch.tensor([1,0],dtype = torch.float, device = device),atol = 0.01)                                                  
 
 def test_k_smooth_fictitious_play_with_PaulTestGame_2x2x2():
-    expected_valuation = run_k_smooth_fictitious_play(3, PaulTestGame(device = device),
+    expected_valuation = run_k_smooth_fictitious_play(3, PaulTestGame(cuda = cuda),
                                      initial_beliefs = [torch.tensor([0.1,0.9], dtype = torch.float, device = device),
                                                         torch.tensor([0.2,0.8], dtype = torch.float, device = device),
                                                         torch.tensor([0.3,0.7], dtype = torch.float, device = device)],
