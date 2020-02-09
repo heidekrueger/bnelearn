8a39d9c62de1899277114711713b6c68f7f2461d
gu74cew <paul.sutterer@tum.de>
2020-02-09 12:50:17 +0100

    Working version. Not generalized. nexT: clean

--- scripts/run_llg_auction.py
+++ scripts/run_llg_auction.py
@@ -27,12 +27,26 @@ from bnelearn.strategy import ClosureStrategy, NeuralNetStrategy
 
 ##%%%%%%%%%%%%%%%%%%%%%%%%%%    Settings
 # device and seed
-cuda = True
-specific_gpu = 3
+# set up matplotlib
+is_ipython = 'inline' in plt.get_backend()
+if is_ipython:
+    from IPython import display
+plt.rcParams['figure.figsize'] = [10, 7]
+    
+cuda = torch.cuda.is_available()
+device = 'cuda' if cuda else 'cpu'
 
-n_runs = 1
+# Use specific cuda gpu if desired (i.e. for running multiple experiments in parallel)
+specific_gpu = 4
+if cuda and specific_gpu:
+    torch.cuda.set_device(specific_gpu)
+
+print(device)
+if cuda: print(torch.cuda.current_device())
+
+n_runs = 10
 seeds = list(range(n_runs))
-epochs = 1000
+epochs = 100
 
 # Logging and plotting
 logging_options = dict(
@@ -45,20 +59,14 @@ logging_options = dict(
 )
 
 # Experiment setting parameters
-#$New: define number of players sharing a model here. -> More genealized.
-players_sharing_model = (2,1)
-players_sharing_model_index = [0]
-for i in range(len(players_sharing_model)-1):
-    players_sharing_model_index.append(
-        players_sharing_model_index[i]+players_sharing_model[i])
-n_players = sum(players_sharing_model)
+n_players = 3
 
 
 auction_mechanism = LLGAuction # FirstPriceSealedBidAuction, VickreyAuction, LLGAuction
-payment_rule =  'vcg'            #'first_price', 'vcg', 'nearest-vcg,...'
+payment_rule =  'nearest_zero'            #'first_price', 'vcg', 'nearest-vcg,...'
 gamma = 0
 valuation_prior = 'uniform' # for now, one of 'uniform' / 'normal', specific params defined in script
-risk = 0.5
+risk = 1.0
 
 if risk == 1.0:
     risk_profile = 'risk_neutral'
@@ -67,10 +75,9 @@ elif risk == 0.5:
 else:
     risk_profile = 'other'
 
+u_lo = 0
 u_hi_local = 1.0
 u_hi_global = 2.0
-
-u_lo = 0
 
 u_his = [u_hi_local, u_hi_local, u_hi_global]
 
@@ -100,7 +107,7 @@ optimizer_hyperparams ={
 }
 
 # plot and log training options
-plot_epoch = 10
+plot_epoch = 100
 plot_points = min(100, batch_size)
 
 # in single item auctions there's only a single input
@@ -116,120 +123,93 @@ n_processes_optimal_strategy = 44 if valuation_prior != 'uniform' and auction_me
 
 ######################### No settings beyond this point ######################
 
-
-# set up matplotlib
-is_ipython = 'inline' in plt.get_backend()
-if is_ipython:
-    from IPython import display
-plt.rcParams['figure.figsize'] = [8, 5]
-
-### set device settings
-if cuda and not torch.cuda.is_available():
-    warnings.warn('Cuda not available. Falling back to CPU!')
-    cuda = False
-device = 'cuda' if cuda else 'cpu'
-
-if cuda and specific_gpu:
-    torch.cuda.set_device(specific_gpu)
-
 ### Set up experiment domain and bidders
-if valuation_prior == 'uniform':
-    u_lo =0
-    u_hi =10
-    common_prior = torch.distributions.uniform.Uniform(low = u_lo, high = u_hi)
+def strat_to_bidder(strategy, batch_size, player_position):
+    return Bidder.uniform(u_lo, u_his[player_position], strategy, player_position=player_position, batch_size = batch_size)
 
-    positive_output_point = u_hi
-
-    def strat_to_bidder(strategy, batch_size=batch_size, player_position=None, cache_actions=False):
-        return Bidder.uniform(u_lo, u_hi, strategy, batch_size = batch_size,
-                              player_position=player_position, cache_actions=cache_actions, risk=risk)
-    plot_xmin = u_lo
-    plot_xmax = u_hi
-    plot_ymin = 0
-    plot_ymax = 10
-
-elif valuation_prior == 'normal':
-    assert risk_profile == 'risk_neutral', "No optimal solution known for risk != 0 with Gaussian priors. " \
-                       + "Comment out this line only if you know what you're doing!"
-    valuation_mean = 10.0
-    valuation_std = 5.0
-    common_prior = torch.distributions.normal.Normal(loc = valuation_mean, scale = valuation_std)
-
-    positive_output_point = valuation_mean
-
-    def strat_to_bidder(strategy, batch_size=batch_size, player_position=None, cache_actions=False):
-        return Bidder.normal(valuation_mean, valuation_std, strategy,
-                             batch_size = batch_size,
-                             player_position=player_position,
-                             cache_actions=cache_actions,
-                             risk = risk)
-
-    plot_xmin = int(max(0, valuation_mean - 3*valuation_std))
-    plot_xmax = int(valuation_mean + 3*valuation_std)
-    plot_ymin = 0
-    plot_ymax = 20 if auction_mechanism == 'first_price' else plot_xmax
-
-else:
-    raise ValueError('Only normal and uniform priors supported by this script.')
-
-def setup_bidders(self):
+def setup_bidders(self, local_model_sharing = True):
     print('Setting up models and bidders...')
-    self.models = [
-        NeuralNetStrategy(
-            input_length, hidden_nodes = hidden_nodes,hidden_activations = hidden_activations,
-            ensure_positive_output = torch.tensor([float(positive_output_point)])
-            ).to(device)
-        for i in range(len(self.players_sharing_model))]
-    self.bidders = []
-    for index in range(len(self.players_sharing_model)):
-        for model in range(self.players_sharing_model[index]):
-            self.bidders.append(
-                strat_to_bidder(self.models[model], batch_size,
-                        self.players_sharing_model_index[index]+model))
+    model_l1 = NeuralNetStrategy(input_length,
+                                 hidden_nodes = hidden_nodes,
+                                 hidden_activations = hidden_activations,
+                                 ensure_positive_output = torch.tensor([float(u_hi_local)])
+                                 ).to(device)
+
+    if not local_model_sharing:
+        model_l2 = NeuralNetStrategy(input_length,
+                                     hidden_nodes = hidden_nodes,
+                                     hidden_activations = hidden_activations,
+                                     ensure_positive_output = torch.tensor([float(u_hi_local)])
+                                     ).to(device)
+
+    # global player
+    model_g = NeuralNetStrategy(input_length,
+                                hidden_nodes = hidden_nodes,
+                                hidden_activations = hidden_activations,
+                                ensure_positive_output = torch.tensor([float(u_hi_global)])
+                                ).to(device)
+    bidder_l1 = strat_to_bidder(model_l1, batch_size, player_position=0)
+    bidder_l2 = strat_to_bidder(model_l1, batch_size, player_position=1) \
+                    if local_model_sharing else strat_to_bidder(model_l2, batch_size, player_position=1)
+    bidder_g  = strat_to_bidder(model_g,  batch_size, player_position=2)
     
+    self.bidders = [bidder_l1, bidder_l2, bidder_g]
     self.n_parameters = [sum([p.numel() for p in model.parameters()]) for model in [b.strategy for b in self.bidders]]
 
     if pretrain_iters > 0:
         print('\tpretraining...')
-        for model in range(len(self.models)):
-            self.models[model].pretrain(self.bidders[self.players_sharing_model_index[model]].valuations, 
-                                        pretrain_iters)
+        for bidder in self.bidders:
+            bidder.strategy.pretrain(bidder.valuations, pretrain_iters)
 
 ### Setup Learning Environment and Learner(s)
 def setup_learning_environment(self):
-    self.env = AuctionEnvironment(self.mechanism, 
+    self.env = AuctionEnvironment(self.mechanism,
                                   agents = self.bidders,
-                                  batch_size = batch_size, 
+                                  batch_size = batch_size,
                                   n_players =n_players,
                                   strategy_to_player_closure = strat_to_bidder)
 def setup_learner(self):
-    self.learners = [
-        ESPGLearner(
-            model = self.models[model_k], environment = self.env, 
-            hyperparams = learner_hyperparams,optimizer_type = optimizer_type, 
-            optimizer_hyperparams = optimizer_hyperparams, 
-            strat_to_player_kwargs = {"player_position":players_sharing_model_index[model_k]})
-        for model_k in range(len(self.models))]
+    learner_l1 = ESPGLearner(model = self.bidders[0].strategy,
+                             environment = self.env,
+                             hyperparams = learner_hyperparams,
+                             optimizer_type = optimizer_type,
+                             optimizer_hyperparams = optimizer_hyperparams,
+                             strat_to_player_kwargs={"player_position":0}
+                 )
 
+    learner_l2 = ESPGLearner(model = self.bidders[1].strategy,
+                             environment = self.env,
+                             hyperparams = learner_hyperparams,
+                             optimizer_type = optimizer_type,
+                             optimizer_hyperparams = optimizer_hyperparams,
+                             strat_to_player_kwargs={"player_position":1}
+                 )
+
+    learner_g = ESPGLearner(model = self.bidders[2].strategy,
+                            environment = self.env,
+                            hyperparams = learner_hyperparams,
+                            optimizer_type = optimizer_type,
+                            optimizer_hyperparams = optimizer_hyperparams,
+                            strat_to_player_kwargs={"player_position":2}
+                )
+    self.learners = [learner_l1, learner_l2, learner_g]
 
 
 
 ### Setup Evaluation
-#$New:TODO: Move optimal solutions to environment!?
 # for evaluation
 def optimal_bid(valuation: torch.Tensor or np.ndarray or float, player_position: int) -> torch.Tensor:
     if not isinstance(valuation, torch.Tensor):
         valuation = torch.tensor(valuation)
-    
+
     # all core-selecting rules are strategy proof for global player:
     if player_position == 2:
         return valuation
-    
-    ## local bidders:
-    
-    if payment_rule == 'vcg':        
+
+    # local bidders:
+    if payment_rule == 'vcg':
         return valuation
-    if payment_rule == 'proxy':
+    if payment_rule in ['proxy', 'nearest_zero']:
         bid_if_positive = 1 + torch.log(valuation * (1.0-gamma) + gamma)/(1.0-gamma)
         return torch.max( torch.zeros_like(valuation), bid_if_positive) 
     if payment_rule == 'nearest_bid':
@@ -240,16 +220,13 @@ def optimal_bid(valuation: torch.Tensor or np.ndarray or float, player_position:
     
     raise ValueError('optimal bid not implemented for other rules')
 
-#TODO: get n_parameters out
-#print('Total parameters: ' + str(n_parameters))
-
 bne_strategies = [
     ClosureStrategy(partial(optimal_bid, player_position=i))
     for i in range(n_players)
 ]
 
 global_bne_env = AuctionEnvironment(
-    auction_mechanism(cuda = cuda),
+    auction_mechanism(cuda = cuda, rule = payment_rule),
     agents = [strat_to_bidder(bne_strategies[i], player_position=i, batch_size=eval_batch_size)
               for i in range(n_players)],
     n_players = n_players,
@@ -265,17 +242,6 @@ print(('Utilities in BNE (sampled):'+ '\t{:.5f}'*n_players + '.').format(*global
 
 eps_abs = lambda us: global_bne_utility_sampled - us
 eps_rel = lambda us: 1- us/global_bne_utility_sampled
-
-#$TODO: What to do with this?
-utilities_vs_bne = torch.tensor([global_bne_env.get_strategy_reward(a.strategy, player_position=i) for i,a in enumerate(global_bne_env.agents)])
-print(('Model utility vs BNE: \t'+'\t{:.5f}'*n_players).format(*utilities_vs_bne))
-
-utilities_learning_env = torch.tensor([global_bne_env.get_strategy_reward(a.strategy, 
-                                                               player_position=i, 
-                                                               draw_valuations = True) 
-                                                               for i,a in enumerate(global_bne_env.agents)])
-print(('Model utility in learning env:'+'\t{:.5f}'*n_players).format(*utilities_learning_env))
-
 
 def setup_eval_environment(self):
     # environment filled with optimal players for logging
@@ -293,34 +259,22 @@ bl2_opt = optimal_bid(vl2_opt, 0).numpy()
 vg_opt = np.linspace(u_lo, u_hi_global, 50)
 bg_opt = optimal_bid(vg_opt, 2).numpy() 
 
-def plot_bid_function(fig, v, b, log_dir, writer=None, e=None,
-                      plot_points=plot_points,
-                      save_vectors_to_disc=logging_options['save_figure_data_to_disc'],
-                      save_png_to_disc = False):
-    
+def plot_bid_function(self, fig, v, b, writer=None, e=None):
     # subsample points and plot
     for i in range(len(v)):
         v[i] = v[i].detach().cpu().numpy()[:plot_points]
         b[i] = b[i].detach().cpu().numpy()[:plot_points]
 
-
     fig = plt.gcf()
     plt.cla()
-    plt.xlim(plot_xmin, plot_xmax)
-    plt.ylim(plot_ymin, plot_ymax)
+    plt.xlim(0, 2)
+    plt.ylim(0, 2)
     plt.xlabel('valuation')
     plt.ylabel('bid')
-    plt.text(plot_xmin + 1, plot_ymax - 1, 'iteration {}'.format(e))
-    #$TODO: Generalize to v and b
+    plt.text(0 + 1, 2 - 1, 'iteration {}'.format(e))
     plt.plot(v[0],b[0], 'bo', vl1_opt, bl1_opt, 'b--', v[1],b[1], 'go', vl2_opt, bl2_opt, 'g--', v[2],b[2], 'ro', vg_opt,bg_opt, 'r--')
-    if is_ipython:
-        display.clear_output(wait=True)
-    #display.display(fig)
-    plt.show()
-    if save_png_to_disc:
-        plt.savefig(os.path.join(log_dir, 'png', f'_{e:05}.png'))
-    if writer:
-        writer.add_figure('eval/bid_function', fig, e)
+    self._process_figure(fig, writer, e)
+
 
 def log_once(self, writer, e):
     """Everything that should be logged only once on initialization."""
@@ -329,26 +283,37 @@ def log_once(self, writer, e):
     writer.add_scalar('debug/model_parameters', sum(self.n_parameters), e)
     writer.add_scalar('debug/eval_batch_size', eval_batch_size, e)
     for a in self.bidders:
-        writer.add_text('hyperparams/neural_net_spec', str(a.strategy), 0)    
+        writer.add_text('hyperparams/neural_net_spec', str(a.strategy), 0)
     #writer.add_scalar('debug/eval_batch_size', eval_batch_size, e)
     #Currently only the first grad is printed. TODO: Generalize
-    writer.add_graph(self.models[0], self.env.agents[0].valuations)
+    writer.add_graph(self.bidders[0].strategy, self.env.agents[0].valuations)
 
-def log_metrics(self, writer, e):
-    writer.add_scalar('eval/utility', self.utility, e)
-    writer.add_scalar('debug/norm_parameter_update', self.update_norm, e)
-    writer.add_scalar('eval/utility_vs_bne', self.utility_vs_bne, e)
-    writer.add_scalar('eval/epsilon_relative', self.epsilon_relative, e)
-    writer.add_scalar('eval/epsilon_absolute', self.epsilon_absolute, e)
+def log_metrics(self, writer, utility, utilities_vs_bne, e):
+    """log scalar for each player. Tensor should be of shape n_players"""
+    epsilons_rel = eps_rel(utilities_vs_bne)
+    epsilons_abs = eps_abs(utilities_vs_bne)
+    
+    # redundant logging of utlities for multiline
+    for i in range(n_players):
+        ## Note: multiline chart capture all tags that match the given beginning of the tag_name,
+        ## i.e. eval/utility will match all of  eval/utility, eval/utility_sp and eval/utlity_vs_bne
+        ## thus self play utility should be named utility_sp to be able to capture it by itself later.
+        writer.add_scalar('eval_players/p{}_utility_sp'.format(i), utility[i], e)
+        writer.add_scalar('eval_players/p{}_utility_vs_bne'.format(i), utilities_vs_bne[i], e)
+        writer.add_scalar('eval_players/p{}_epsilon_absolute'.format(i), epsilons_abs[i], e)
+        writer.add_scalar('eval_players/p{}_epsilon_relative'.format(i), epsilons_rel[i], e)
+    
+    writer.add_scalar('eval/epsilon_relative', epsilons_rel.mean(),e)
+    writer.add_scalar('debug/epsilon_absolute', epsilons_abs.mean(),e)
 
 # TODO: deferred until writing logger
 def log_hyperparams(self, writer, e):
     """Everything that should be logged on every learning_rate updates"""
-#     writer.add_scalar('hyperparams/batch_size', batch_size, e)
-#     writer.add_scalar('hyperparams/learning_rate', learning_rate, e)
-#     writer.add_scalar('hyperparams/momentum', momentum, e)
-#     writer.add_scalar('hyperparams/sigma', sigma, e)
-#     writer.add_scalar('hyperparams/n_perturbations', n_perturbations, e)
+    writer.add_scalar('hyperparams/batch_size', batch_size, e)
+    writer.add_scalar('hyperparams/learning_rate', optimizer_hyperparams['lr'], e)
+    #writer.add_scalar('hyperparams/momentum', optimizer_hyperparams['momentum'], e)
+    writer.add_scalar('hyperparams/sigma', learner_hyperparams['sigma'], e)
+    writer.add_scalar('hyperparams/n_perturbations', learner_hyperparams['population_size'], e)
 
 def setup_custom_scalar_plots(writer):    
     ## define layout first, then call add_custom_scalars once
@@ -365,15 +330,12 @@ def setup_custom_scalar_plots(writer):
 def training_loop(self, writer, e):
 
     setup_custom_scalar_plots(writer)
-    
     overhead_mins = 0
     torch.cuda.empty_cache()
     self.log_once(writer, 0)
     self.log_hyperparams(writer, 0)
     fig = plt.figure()
     
-    
-    # plot current function output
     # plot current function output
     v = []
     b = []
@@ -381,8 +343,8 @@ def training_loop(self, writer, e):
         self.bidders[index].draw_valuations_()
         v.append(self.bidders[index].valuations)
         b.append(self.bidders[index].get_action())
-           
-    plot_bid_function(self.fig, v, b, self.log_dir, writer,e)     
+    if e == 0:       
+        self.plot(self.fig, v, b, writer,e)     
     # always: do optimizer step
     utilities = [
         learner.update_strategy_and_evaluate_utility()
@@ -393,13 +355,15 @@ def training_loop(self, writer, e):
     start_time = timer()
     utilities = torch.tensor(utilities)
     utilities_vs_bne = torch.tensor([global_bne_env.get_strategy_reward(a.strategy, player_position=i) for i,a in enumerate(self.env.agents)])
+
+    
     self.log_metrics(writer, utilities, utilities_vs_bne, e)
     if e % self._logging_options['plot_epoch'] == 0:
         
         # plot current function output
         v = []
         b = []
-        for index in self.players_sharing_model_index:
+        for index in range(n_players):
             self.bidders[index].draw_valuations_()
             v.append(self.bidders[index].valuations)
             b.append(self.bidders[index].get_action())
@@ -408,8 +372,7 @@ def training_loop(self, writer, e):
         print(('Epoch: {}: Model utility in learning env:'+'\t{:.5f}'*n_players).format(e, *utilities))            
         #print("Epoch {}: \tutilities: \t p0: {:.3f} \t p1: {:.3f}".format(e, utility_0, utility_1))
 
-        plot_bid_function(self.fig, v, b, self.log_dir, writer,e,
-                          save_png_to_disc=self.save_figure_to_dis)            
+        self.plot(self.fig, v, b, writer,e)            
     
     elapsed = timer() - start_time
     overhead_mins = overhead_mins + elapsed/60
@@ -438,8 +401,8 @@ def run(seed, run_comment, epochs):
     exp = AuctionExperiment(
         name = ['single_item', auction_mechanism.name, valuation_prior,
                 'symmetric', risk_profile, str(n_players)+'p'],
-        mechanism = auction_mechanism(cuda = cuda),
-        players_sharing_model = players_sharing_model,
+        mechanism = auction_mechanism(cuda = cuda, rule = payment_rule),
+        n_players = n_players,
         logging_options = logging_options)
 
     exp.run(epochs, run_comment)
