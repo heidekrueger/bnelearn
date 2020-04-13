c74980e40e547f1c159e5b743965113190b183bf
Stefan Heidekrüger <stefan.heidekrueger@tum.de>
2020-04-13 16:35:57 +0200

    added test

--- bnelearn/tests/test_experiment_runtimes/test_single_item.py
+++ bnelearn/tests/test_experiment_runtimes/test_single_item.py
@@ -29,41 +29,53 @@
 # gpu_config = GPUController(specific_gpu=0)
 
 
+# learner_hyperparams = {
+#     'population_size': 64,
+#     'sigma': 1.,
+#     'scale_sigma_by_model_size': True
+# }
+# optimizer_hyperparams = {
+#     'lr': 3e-3
+# }
+# experiment_params = {
+#     # 'model_sharing': set in test functions below
+#     'u_lo': 0,
+#     'u_hi': 1,
+#     'payment_rule': 'first_price',
+#     'risk': 1.0,
+#     'regret_batch_size': 2**8,
+#     'regret_grid_size': 2**8
+# }
+
+# input_length = 1
+# hidden_nodes = [5, 5]
+# hidden_activations = [nn.SELU(), nn.SELU()]
+
+# l_config = LearningConfiguration(learner_hyperparams=learner_hyperparams,
+#                                 optimizer_type='adam',
+#                                 optimizer_hyperparams=optimizer_hyperparams,
+#                                 input_length=input_length,
+#                                 hidden_nodes=hidden_nodes,
+#                                 hidden_activations=hidden_activations,
+#                                 pretrain_iters=50, batch_size=2 ** 18,
+#                                 eval_batch_size=2 ** 18,
+#                                 cache_eval_actions=False)
+
 
 # def test_uniform_symmetric_model_sharing():
-#     learner_hyperparams = {
-#         'population_size': 64,
-#         'sigma': 1.,
-#         'scale_sigma_by_model_size': True
-#     }
-#     optimizer_hyperparams = {
-#         'lr': 3e-3
-#     }
-#     experiment_params = {
-#         'model_sharing': True,
-#         'u_lo': 0,
-#         'u_hi': 1,
-#         'payment_rule': 'first_price',
-#         'risk': 1.0,
-#         'regret_batch_size': 2**8,
-#         'regret_grid_size': 2**8
-#     }
-
-#     input_length = 1
-#     hidden_nodes = [5, 5]
-#     hidden_activations = [nn.SELU(), nn.SELU()]
-
-#     l_config = LearningConfiguration(learner_hyperparams=learner_hyperparams,
-#                                     optimizer_type='adam',
-#                                     optimizer_hyperparams=optimizer_hyperparams,
-#                                     input_length=input_length,
-#                                     hidden_nodes=hidden_nodes,
-#                                     hidden_activations=hidden_activations,
-#                                     pretrain_iters=50, batch_size=2 ** 18,
-#                                     eval_batch_size=2 ** 18,
-#                                     cache_eval_actions=False)
+#     experiment_params['model_sharing'] = True
 
 #     for n in [2,3]:
 #         experiment_params['n_players'] = n
-#         experiment = UniformSymmetricPriorSingleItemExperiment(experiment_params, gpu_config=gpu_config, l_config=l_config)
+#         experiment = UniformSymmetricPriorSingleItemExperiment(
+#             experiment_params, gpu_config=gpu_config, l_config=l_config)
+#         experiment.run(epochs=101, n_runs=2)
+
+# def test_uniform_symmetric_without_model_sharing():
+#     experiment_params['model_sharing'] = False
+    
+#     for n in [2]:
+#         experiment_params['n_players'] = n
+#         experiment = UniformSymmetricPriorSingleItemExperiment(
+#             experiment_params, gpu_config=gpu_config, l_config=l_config)
 #         experiment.run(epochs=101, n_runs=2)
