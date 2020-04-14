6a355d08fcb996ab88c1da0b2e5e3f310a738035
gu74cew <paul.sutterer@tum.de>
2020-04-14 12:55:18 +0200

    using fire with functions

--- bnelearn/experiment/run_experiment.py
+++ bnelearn/experiment/run_experiment.py
@@ -2,6 +2,7 @@ import sys
 sys.path.append('..')
 import torch
 import torch.nn as nn
+import fire
 
 from bnelearn.experiment.gpu_controller import GPUController
 from bnelearn.experiment.learning_configuration import LearningConfiguration
@@ -11,55 +12,60 @@ from bnelearn.experiment.single_item_experiment import UniformSymmetricPriorSing
 
 from bnelearn.experiment.combinatorial_experiment import LLGExperiment, LLLLGGExperiment
 import warnings
-gpu_config = GPUController(specific_gpu=1)
 
-learner_hyperparams = {
-    'population_size': 128,
-    'sigma': 1.,
-    'scale_sigma_by_model_size': True
-}
+#TODO: Using locals() to directly create the dict 
+# (https://stackoverflow.com/questions/2521901/get-a-list-tuple-dict-of-the-arguments-passed-to-a-function)
+# fine with you? 
+def run_single_item_uniform_symmetric(n_runs, n_epochs, 
+                                      n_players: [int], model_sharing=True, u_lo=0, u_hi=1, 
+                                      payment_rule='first_price', risk=1.0, regret_batch_size=2**8, regret_grid_size=2**8,
+                                      specific_gpu=1):
+    experiment_params = locals()
+    input_length = 1
+    experiment_class = UniformSymmetricPriorSingleItemExperiment
+    return n_runs, n_epochs, n_players, specific_gpu, input_length, experiment_class, experiment_params
 
-optimizer_hyperparams = {
-    'lr': 3e-3
-}
-
-# experiment_params = {
-#         'model_sharing': False,
-#         'u_lo': 0,
-#         'u_hi': 1,
-#         'payment_rule': 'first_price',
-#         'risk': 1.0,
-#         'regret_batch_size': 2**8,
-#         'regret_grid_size': 2**8
-#     }
-experiment_params = {
-        'model_sharing': True,
-        'valuation_mean': 15,
-        'valuation_std': 5,
-        'payment_rule': 'first_price',
-        'risk': 1.0,
-        'regret_batch_size': 2**8,
-        'regret_grid_size': 2**8
-    }
-
-input_length = 1
-hidden_nodes = [5, 5, 5]
-hidden_activations = [nn.SELU(), nn.SELU(), nn.SELU()]
-
-l_config = LearningConfiguration(learner_hyperparams=learner_hyperparams,
-                                 optimizer_type='adam',
-                                 optimizer_hyperparams=optimizer_hyperparams,
-                                 input_length=input_length,
-                                 hidden_nodes=hidden_nodes,
-                                 hidden_activations=hidden_activations,
-                                 pretrain_iters=300, batch_size=2 ** 18,
-                                 eval_batch_size=2 ** 15,
-                                 cache_eval_actions=True)
-
-
-#warnings.simplefilter("ignore")
-for i in [2]:
-    experiment_params['n_players'] = i
-    experiment = GaussianSymmetricPriorSingleItemExperiment(experiment_params, gpu_config=gpu_config, l_config=l_config)
-    experiment.run(epochs=101, n_runs=2)
+def run_single_item_gaussian_symmetric(n_runs, n_epochs, 
+                                       n_players: [int], model_sharing=True, valuation_mean=15, valuation_std=10, 
+                                       payment_rule='first_price', risk=1.0, regret_batch_size=2**8, regret_grid_size=2**8,
+                                       specific_gpu=1):
+    experiment_params = locals()
+    input_length = 1
+    experiment_class = GaussianSymmetricPriorSingleItemExperiment
+    return n_runs, n_epochs, n_players, specific_gpu, input_length, experiment_class, experiment_params
                             
+
+
+if __name__ == '__main__':
+    #n_runs, n_epochs, n_players, specific_gpu, input_length, experiment_class, experiment_params = run_single_item_gaussian_symmetric(1,20,[2,3])
+    n_runs, n_epochs, n_players, specific_gpu, input_length, experiment_class, experiment_params = fire.Fire()
+
+    gpu_config = GPUController(specific_gpu=specific_gpu)
+
+    # Learner Config
+    hidden_nodes = [5, 5, 5]
+    hidden_activations = [nn.SELU(), nn.SELU(), nn.SELU()]
+
+    learner_hyperparams = {
+        'population_size': 128,
+        'sigma': 1.,
+        'scale_sigma_by_model_size': True
+    }
+    optimizer_hyperparams = {
+        'lr': 3e-3
+    }
+    #TODO: input length must result from experiment
+    l_config = LearningConfiguration(learner_hyperparams=learner_hyperparams,
+                                    optimizer_type='adam',
+                                    optimizer_hyperparams=optimizer_hyperparams,
+                                    input_length=input_length,
+                                    hidden_nodes=hidden_nodes,
+                                    hidden_activations=hidden_activations,
+                                    pretrain_iters=300, batch_size=2 ** 18,
+                                    eval_batch_size=2 ** 15,
+                                    cache_eval_actions=True)
+
+    for i in n_players:
+        experiment_params['n_players'] = i
+        experiment = experiment_class(experiment_params, gpu_config, l_config)
+        experiment.run(epochs=n_epochs, n_runs=n_runs)
