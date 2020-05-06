cc97ebf93dcfd40e64fcc2e37ccc40be2c098957
Stefan Heidekrüger <stefan.heidekrueger@tum.de>
2020-05-06 19:02:37 +0200

    implemented custom summaryWriter

--- bnelearn/util/logging.py
+++ bnelearn/util/logging.py
@@ -1,10 +1,14 @@
 """This module contains utilities for logging of experiments"""
 
 import os
+import time
+from typing import List
 from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
+from torch.utils.tensorboard.writer import SummaryWriter, FileWriter, scalar
 import pandas as pd
 import warnings
 import matplotlib.pyplot as plt
+import torch
 
 
 # based on https://stackoverflow.com/a/57411105/4755970
@@ -72,3 +76,61 @@ def process_figure(fig, epoch=None, figure_name='plot', tb_group='eval',
 
     if display:
         plt.show()
+
+
+class CustomSummaryWriter(SummaryWriter):
+    """Extends SummaryWriter with a method to add multiple scalars in the way
+    that we intend. The original SummaryWriter can either add a single scalar at a time
+    or multiple scalars, but in the latter case, multiple runs are created without
+    the option to control these.
+    """
+
+    def add_metrics_dict(self, metrics_dict: dict, run_suffices: List[str], global_step=None, walltime=None,
+                        group_prefix: str = None ):
+        """         
+        Args:
+            metric_dict (dict): A dict of metrics. Keys are tag names, values are values.
+                values can be float, List[float] or Tensor.
+                When List or (nonscalar) tensor, the length must match n_models
+            run_suffices (List[str]): if each value in metrics_dict is scalar, doesn't need to be supplied.
+                When metrics_dict contains lists/iterables, they must all have the same length which should be equal to
+                the length of run_suffices
+        """
+        torch._C._log_api_usage_once("tensorboard.logging.add_scalar")
+        walltime = time.time() if walltime is None else walltime
+        fw_logdir = self._get_file_writer().get_logdir()
+
+        if run_suffices is None:
+            run_suffices = []
+
+        l = len(run_suffices)
+        print(l)
+
+
+        for key, vals in metrics_dict.items():
+            tag = key if not group_prefix else group_prefix + '/' + key
+
+            if isinstance(vals, float) or isinstance(vals, int) or (
+                torch.is_tensor(vals) and vals.size() in {torch.Size([]), torch.Size([1])}
+                ):
+                """Only a single value --> log directly in main run""" 
+                
+                self.add_scalar(tag, vals, global_step, walltime)
+            elif len(vals) == l:
+                """Log each into a run with its own prefix."""
+                for suffix, scalar_value in zip(run_suffices, vals):
+                    fw_tag = fw_logdir + "/" + suffix.replace("/", "_")
+
+                    if fw_tag in self.all_writers.keys():
+                        fw = self.all_writers[fw_tag]
+                    else:
+                        fw = FileWriter(fw_tag, self.max_queue, self.flush_secs,
+                                        self.filename_suffix)
+                        self.all_writers[fw_tag] = fw
+                    # Not using caffe2 -->following line is commented out from original SummaryWriter implementation
+                    # if self._check_caffe2_blob(scalar_value):
+                    #     scalar_value = workspace.FetchBlob(scalar_value)
+                    fw.add_summary(scalar(tag, scalar_value),
+                                global_step, walltime)
+            else:
+                raise ValueError('Got list of invalid length.')
