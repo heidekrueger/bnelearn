46bb430a36542401d0bfb33671504bd667b2554b
gu74cew <paul.sutterer@tum.de>
2020-05-06 10:52:46 +0200

    fix

--- bnelearn/experiment/experiment.py
+++ bnelearn/experiment/experiment.py
@@ -486,7 +486,7 @@ class Experiment(ABC):
         if self.logging_config.log_metrics['opt'] and hasattr(self, 'bne_env'):
             # dim: [points, bidders, items]
             self.v_opt = torch.stack(
-                [b.draw_values_grid__(self.plot_points)
+                [b.draw_values_grid_(self.plot_points)
                  for b in [self.bne_env.agents[i[0]] for i in self._model2bidder]],
                 dim=1
             )
@@ -496,7 +496,7 @@ class Experiment(ABC):
                 dim=1
             )
             if self.v_opt.shape[0] != self.plot_points:
-                print('´plot_points´ changed due to ´draw_values_grid__´')
+                print('´plot_points´ changed due to ´draw_values_grid_´')
                 self.plot_points = self.v_opt.shape[0]
 
         is_ipython = 'inline' in plt.get_backend()
