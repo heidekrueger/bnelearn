0dce035667b99b992370f8de6d564de76dcd3fc1
gu74cew <paul.sutterer@tum.de>
2020-05-07 08:29:11 +0200

    improved fire.fire

--- scripts/run_experiment.py
+++ scripts/run_experiment.py
@@ -17,7 +17,11 @@ from bnelearn.experiment.single_item_experiment import UniformSymmetricPriorSing
 from bnelearn.experiment.combinatorial_experiment import LLGExperiment, LLLLGGExperiment
 from bnelearn.experiment.multi_unit_experiment import MultiUnitExperiment, SplitAwardExperiment
 import warnings
-from bnelearn.experiment import presets
+from bnelearn.experiment.presets import (llg, llllgg, multiunit,
+                                         single_item_asymmetric_uniform,
+                                         single_item_gaussian_symmetric,
+                                         single_item_uniform_symmetric,
+                                         splitaward)
 
 from dataclasses import dataclass, field, asdict
 
@@ -29,33 +33,33 @@ if __name__ == '__main__':
     fire.Fire() asks you to decide for one of the experiments defined above
     by writing its name and define the required (and optional) parameters
     e.g.:
-        presets.experiment.py presets.single_item_uniform_symmetric 1 20 [2,3] 'first_price'
+        experiment.py single_item_uniform_symmetric 1 20 [2,3] 'first_price'
 
     alternatively instead of fire.Fire() use, e.g.:
-        presets.single_item_uniform_symmetric(1,20,[2,3],'first_price')
+        single_item_uniform_symmetric(1,20,[2,3],'first_price')
 
     '''
     enable_logging = True
 
+    running_configuration, logging_configuration, experiment_configuration, experiment_class = \
+        fire.Fire()
     # running_configuration, logging_configuration, experiment_configuration, experiment_class = \
-    #     fire.Fire()
-    # running_configuration, logging_configuration, experiment_configuration, experiment_class = \
-    #     presets.single_item_uniform_symmetric(2, 10, [2], 'first_price', model_sharing=True, logging=enable_logging)
+    #     single_item_uniform_symmetric(2, 10, [2], 'first_price', model_sharing=True, logging=enable_logging)
     # logging_configuration.save_tb_events_to_binary_detailed = True
     # logging_configuration.save_tb_events_to_csv_detailed = True
 
-    running_configuration, logging_configuration, experiment_configuration, experiment_class = \
-        presets.single_item_gaussian_symmetric(2,110, [2], 'second_price', logging=enable_logging)
+    # running_configuration, logging_configuration, experiment_configuration, experiment_class = \
+    #     single_item_gaussian_symmetric(2,110, [2], 'second_price', logging=enable_logging)
     # running_configuration, logging_configuration, experiment_configuration, experiment_class =\
-    #    presets.llg(2,10,'nearest_zero',specific_gpu=1, logging=enable_logging)
+    #    llg(2,10,'nearest_zero',specific_gpu=1, logging=enable_logging)
     # running_configuration, logging_configuration, experiment_configuration, experiment_class = \
-    #    presets.llllgg(2,10,'first_price',model_sharing=False, logging=enable_logging)
+    #    llllgg(2,10,'first_price',model_sharing=False, logging=enable_logging)
     # running_configuration, logging_configuration, experiment_configuration, experiment_class = \
-    #   presets.multiunit(n_runs=2, n_epochs=10, n_players=[2], n_units=2, payment_rule='first_price', logging=enable_logging)
+    #   multiunit(n_runs=2, n_epochs=10, n_players=[2], n_units=2, payment_rule='first_price', logging=enable_logging)
     # running_configuration, logging_configuration, experiment_configuration, experiment_class = \
-    #   presets.splitaward(1, 10, [2], logging=enable_logging)
+    #   splitaward(1, 10, [2], logging=enable_logging)
     # running_configuration, logging_configuration, experiment_configuration, experiment_class = \
-    #    presets.single_item_asymmetric_uniform(n_runs=1, n_epochs=10, logging=enable_logging)
+    #    single_item_asymmetric_uniform(n_runs=1, n_epochs=10, logging=enable_logging)
 
 
     gpu_configuration = GPUController(specific_gpu=running_configuration.specific_gpu)
