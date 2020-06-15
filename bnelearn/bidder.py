major fix: welfare calculation

bnelearn/bidder.py
@@ -406,6 +406,7 @@ class CombinatorialItemBidder(Bidder):
         self.n_collections = 2 # TODO make variable
         super().__init__(value_distribution=value_distribution, strategy=strategy,
                          n_items=n_items, **kwargs)
+        self.n_bundles = self.n_items
 
     def draw_valuations_(self):
         """
@@ -426,10 +427,14 @@ class CombinatorialItemBidder(Bidder):
         vals = torch.zeros_like(self.valuations).repeat(1, self.n_collections) \
             .view(self.valuations.shape[0], self.n_items, self.n_collections)
         for i in range(self.n_collections):
-            vals[:,:,i] = additive(super().draw_valuations_())
+            vals[:, :, i] = additive(super().draw_valuations_())
         self.valuations = vals.max(dim=2)[0]
 
         return self.valuations
+
+    def get_valuation_grid(self, n_points, extended_valuation_grid=False, n_dimensions=None):
+        grid = super().get_valuation_grid(n_points, extended_valuation_grid, n_dimensions)
+        return grid[:, :self.n_bids]
 
     def get_welfare(self, allocations, valuations=None):
         """
@@ -447,8 +452,15 @@ class CombinatorialItemBidder(Bidder):
 
         # check if allocation is only based on subset of all bundles
         if allocations.shape[-1] < valuations.shape[-1]:
-            # cut off valuations for bundles which were not for sale
-            valuations = valuations[..., :allocations.shape[-1]]
+            """
+            Here we transform the allocation of (perhaps multiple) items to a bundle allocation that
+            has a larger dimension and contains at most one `1` per batch.
+            """
+            i = allocations * (2**torch.arange(self.n_bids, device=allocations.device).unsqueeze(0))
+            i = i.sum(1)[None].t_().to(int)
+            allocations = torch.zeros((allocations.shape[0], self.n_bundles+1), device=allocations.device)
+            allocations.scatter_(1, i, 1)
+            allocations = allocations[:, 1:] # cut off when no all zeros
 
         item_dimension = valuations.dim() - 1
         welfare = (valuations * allocations).sum(dim=item_dimension)

bnelearn/environment.py
@@ -322,7 +322,7 @@ class AuctionEnvironment(Environment):
 
         def get_allo(item_allo):
             """
-            Based on a item allocations return the correspunding bundle allocations
+            Based on an item allocations returns the correspunding bundle allocations
             """
             a = torch.zeros((n_players, 2**n_items - 1), dtype=int)
             for player_position in range(n_players):
@@ -342,9 +342,9 @@ class AuctionEnvironment(Environment):
         # brute force: all combinations for each batch
         allocations = possible_allocations.repeat(self.batch_size, 1, 1, 1).view(
             self.batch_size, n_players**n_items, n_players, n_bundles
-        )
+        ) # shape (batch_size, possible_combinations, n_players, n_bundles)
 
-        # calculate walfare
+        # calculate welfare
         welfares = torch.zeros_like(allocations[:, :, :, 0], dtype=torch.float32)
         for i, agent in enumerate(self.agents):
             agent_valuations = agent.valuations.repeat(1, 1, n_players**n_items).view(
@@ -358,7 +358,8 @@ class AuctionEnvironment(Environment):
         return max_welfares
 
     def get_PoA(self, allocations):
-        """Returns (Bayesian) Price of Anarchy
+        """
+        Returns (Bayesian) Price of Anarchy
         """
 
         max_welfares = self.get_welfare_max().mean()

bnelearn/util/metrics.py
@@ -149,7 +149,7 @@ def ex_post_util_loss(mechanism: Mechanism, bid_profile: torch.Tensor, bidder: B
 
 def ex_interim_util_loss(mechanism: Mechanism, bid_profile: torch.Tensor,
                          agent: Bidder, agent_valuation: torch.Tensor,
-                         grid: torch.Tensor, half_precision = False):
+                         grid: torch.Tensor, n_bundles=None, half_precision=False):
     """
     Estimates a bidder's util_loss/utility loss in the current bid_profile, i.e. the potential benefit of deviating from
     the current strategy, evaluated at each point of the agent_valuations.
@@ -187,6 +187,9 @@ def ex_interim_util_loss(mechanism: Mechanism, bid_profile: torch.Tensor,
     batch_size, n_players, n_items = bid_profile.shape # pylint: disable=unused-variable
     grid_size, _ = grid.shape
 
+    if n_bundles is None:
+        n_bundles = n_items
+
     ### Evaluate alternative bids on grid
     bid_profile = _create_grid_bid_profiles(player_position, grid, bid_profile_origin) # grid_size x n_players x n_items
 
@@ -204,7 +207,7 @@ def ex_interim_util_loss(mechanism: Mechanism, bid_profile: torch.Tensor,
     # Calculate realized valuations given allocation
     try:
         # valuation is batch x items
-        v_i = agent_valuation.repeat(1, grid_size * batch_size).view(batch_size, grid_size * batch_size, n_items)
+        v_i = agent_valuation.repeat(1, grid_size * batch_size).view(batch_size, grid_size * batch_size, n_bundles)
         p_i = p_i.repeat(batch_size, 1)
 
         ## Calculate utilities
@@ -226,7 +229,7 @@ def ex_interim_util_loss(mechanism: Mechanism, bid_profile: torch.Tensor,
             # valuations sequential
             u_i_alternative = torch.zeros(batch_size, device=p_i.device)
             for idx in tqdm(range(batch_size)):
-                v_i = agent_valuation[idx].repeat(1, grid_size * batch_size).view(batch_size * grid_size, n_items)
+                v_i = agent_valuation[idx].repeat(1, grid_size * batch_size).view(batch_size * grid_size, n_bundles)
                 ## Calculate utilities
                 u_i_alternative_v = agent.get_counterfactual_utility(a_i, p_i, v_i)
                 u_i_alternative_v = u_i_alternative_v.view(grid_size, batch_size) #(grid x batch)
@@ -253,7 +256,7 @@ def ex_interim_util_loss(mechanism: Mechanism, bid_profile: torch.Tensor,
     p_i = payments[:, player_position].view(batch_size * batch_size)
 
     ## Calculate realized valuations given allocation
-    v_i = agent_valuation.repeat(1, batch_size).view(batch_size * batch_size, n_items)
+    v_i = agent_valuation.repeat(1, batch_size).view(batch_size * batch_size, n_bundles)
 
     ## Calculate utilities
     u_i_actual = agent.get_counterfactual_utility(a_i, p_i, v_i).view(batch_size, batch_size)