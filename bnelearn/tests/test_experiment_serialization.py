aacedf3004a9ccabbe17be08c14fd2ac3b2b1f3a
Gleb Kilichenko <glebk98@gmail.com>
2020-06-17 16:57:04 +0200

    test fix

--- bnelearn/tests/test_experiment_serialization.py
+++ bnelearn/tests/test_experiment_serialization.py
@@ -10,8 +10,9 @@ def compare_two_experiments(exp1: Experiment, exp2: Experiment) -> bool:
     return True
 
 
-def test_experiments_equility():
-    log_root_dir = os.path.join(os.path.expanduser('~'), 'Projects/bnelearn', 'experiments')
+def test_experiments_equality():
+    # Not sure about the proper path to use
+    log_root_dir = os.path.join(os.path.expanduser('~'), 'bnelearn', 'experiments')
     experiment_config, experiment_class = ConfigurationManager(experiment_type='single_item_uniform_symmetric') \
         .get_config(save_tb_events_to_csv_detailed=True, log_root_dir=log_root_dir)
     # some parts of the config are changed in the children of the experiment class, so it's necessary to instantiate it
@@ -23,12 +24,11 @@ def test_experiments_equility():
 
     retrieved_experiment = retrieved_experiment_class(retrieved_experiment_config)
     equality = compare_two_experiments(retrieved_experiment, experiment)
-    os.remove(os.path.join(log_root_dir, logging._configurations_f_name))
+    path = os.path.join(log_root_dir, logging._configurations_f_name)
+    if os.path.exists(path):
+        os.remove(path)
 
     assert equality
-
-
-
 
 # experiment_config, experiment_class = ConfigurationManager(experiment_type='single_item_gaussian_symmetric') \
 #     .get_config(log_root_dir=log_root_dir)
