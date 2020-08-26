5c029d9f54319f27070ea423cdbd57e201c82f9f
Stefan Heidekrüger <stefan.heidekrueger@tum.de>
2020-08-26 14:03:31 +0200

    Apply 1 suggestion(s) to 1 file(s)

--- bnelearn/util/metrics.py
+++ bnelearn/util/metrics.py
@@ -244,9 +244,6 @@ def ex_interim_util_loss(mechanism: Mechanism, bid_profile: torch.Tensor,
                 # avg per bid
                 u_i_alternative_v = torch.mean(u_i_alternative_v, 1)
                 # max per valuations
-                # NOTE: item here is temporary fix for memory leak problem in risk-averse setting.
-                # The actual underlying bug is more complicated, but we'll fix that in master, not in this
-                # (i.e. Journal experiment) branch!
                 u_i_alternative[idx] =  torch.max(u_i_alternative_v, 0)[0]
 
                 # clean up
