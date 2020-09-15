6c3b8a203571517740cf5df5694e53c0d633ef76
kohring <nils.kohring@in.tum.de>
2020-09-15 19:06:15 +0200

    typo fix

--- bnelearn/tests/test_util_loss_estimator.py
+++ bnelearn/tests/test_util_loss_estimator.py
@@ -171,7 +171,8 @@ def test_ex_interim_util_loss_estimator_truthful(rule, mechanism, bid_profile, b
 
     for i in range(n_bidders):
         # TODO current problem: cannot redraw opponents valuations as their batch
-        #      size is too small: 1. 
+        #      size is too small: 1.
+        pass
         # util_loss = metrics.ex_interim_util_loss(env, i, batch_size, grid_size, opponent_batch_size)
         # assert torch.allclose(util_loss.mean(), expected_util_loss[i, 0], atol = 1), \
         #     "Unexpected avg util_loss {}".format(util_loss.mean() - expected_util_loss[i, 0])
