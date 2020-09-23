0d4540fa6086b137c3fbf798c27b33dc7baa8339
kohring <nils.kohring@in.tum.de>
2020-09-23 12:17:14 +0200

    Added database basic test

---
+++ bnelearn/tests/test_bne_database.py
@@ -0,0 +1,31 @@
+"""Testing correctness of the BNE utilities database."""
+
+import pytest
+import os
+import sys
+
+sys.path.append(os.path.realpath('.'))
+sys.path.append(os.path.join(os.path.expanduser('~'), 'bnelearn'))
+
+from bnelearn.experiment.configuration_manager import ConfigurationManager
+from bnelearn.util.logging import access_bne_utility_database
+
+
+def test_bne_utility_database():
+    """Testing correctness of the BNE utilities database."""
+    specific_gpu = 0
+
+    experiment_config, experiment_class = ConfigurationManager(experiment_type='llg') \
+        .get_config(
+            specific_gpu=specific_gpu,
+            pretrain_iters=0,
+            batch_size=2,
+            enable_logging=False,
+        )
+
+    experiment = experiment_class(experiment_config)
+    experiment.logging.eval_batch_size = 2
+    experiment.logging.util_loss_batch_size = 2
+    experiment.logging.util_loss_grid_size = 2
+    experiment._setup_eval_environment() #pylint: disable=protected-access
+    access_bne_utility_database(experiment, experiment.bne_utilities_new_sample)
