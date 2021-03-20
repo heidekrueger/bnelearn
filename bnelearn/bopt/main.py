381a2e40814c168bd51427a736b75911e5ad2543
Gleb <glebk98@gmail.com>
2021-03-20 14:36:54 +0100

    bopt setup

---
+++ bnelearn/bopt/main.py
@@ -0,0 +1,33 @@
+from bayes_opt import BayesianOptimization
+
+def black_box_function(x, y):
+    """Function with unknown internals we wish to maximize.
+
+    This is just serving as an example, for all intents and
+    purposes think of the internals of this function, i.e.: the process
+    which generates its output values, as unknown.
+    """
+    return -x ** 2 - (y - 1) ** 2 + 1
+
+
+
+# Bounded region of parameter space
+pbounds = {'x': (2, 4), 'y': (-3, 3)}
+
+optimizer = BayesianOptimization(
+    f=black_box_function,
+    pbounds=pbounds,
+    verbose=2, # verbose = 1 prints only when a maximum is observed, verbose = 0 is silent
+    random_state=1,
+)
+
+optimizer.maximize(
+    init_points=2,
+    n_iter=3,
+)
+
+
+print(optimizer.max)
+
+for i, res in enumerate(optimizer.res):
+    print("Iteration {}: \n\t{}".format(i, res))
