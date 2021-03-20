b06a0fa0669cbafc40273db62cbfd0f64ae3fccb
Stefan Heidekrüger <stefan.heidekrueger@tum.de>
2021-03-20 01:29:35 +0100

    uniform and gaussian ipv samplers + tests

---
+++ bnelearn/tests/test_valuation_samplers.py
@@ -0,0 +1,132 @@
+""" This pytest test file checks whether valuation and observation samplers have the 
+expected behaviour"""
+
+import torch
+import numpy as np
+
+import bnelearn.valuation_sampler as vs
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+n_players = 3
+
+batch_size = 2**18
+alternative_batch_size = 2**15
+
+# parameters for tests with uniform distributions
+u_lo = 0.
+u_hi = 10.
+u_mean = 5.
+u_std = 2.8868
+
+
+# parameters for tests with Normal distributions
+n_mean = 10.
+# we clip vals at 0, so make sure std is small to not affect sample mean!
+n_std = 3.
+
+# helper functions
+def correlation(valuation_profile):
+    """Pearson correlation between valuations of bidders.
+    
+    valuation_profile should be (batch x n_players x 1)
+    """
+    assert valuation_profile.dim() == 3, "invalid valuation profile."
+    assert valuation_profile.shape[2] == 1, "correlation matrix only valid for 1-d valuations"
+
+    return torch.from_numpy(
+        np.corrcoef(valuation_profile.squeeze(-1).t().cpu())
+        ).float() #numpy is float 64
+
+def check_validity(valuation_profile, expected_shape, 
+                   expected_mean, expected_std, expected_correlation=None):
+    assert valuation_profile.dim() == 3, "invalid number of dimensions!"
+    assert valuation_profile.shape == expected_shape, "invalid shape!"
+    assert torch.allclose(valuation_profile.mean(dim=0), expected_mean.to(device), atol=0.02), \
+        "unexpected sample mean!"
+    assert torch.allclose(valuation_profile.std(dim=0), expected_std.to(device), atol=0.02), \
+        "unexpected sample variance!"
+    if expected_correlation is not None:
+        assert torch.allclose(correlation(valuation_profile), expected_correlation.cpu(), atol=0.01), \
+            "unexpected correlation between bidders!"
+
+
+def test_uniform_symmetric_ipv():
+    """Test the UniformSymmetricIPVSampler."""
+
+    ### test with valuation size 1.
+    s = vs.UniformSymmetricIPVSampler(u_lo, u_hi, n_players, 1, batch_size)
+
+
+    v = s.sample()
+    assert v.device.type == device, "Standard device should be cuda, if available!"
+
+    check_validity(v,
+                   expected_shape= torch.Size([batch_size, n_players, 1]),
+                   expected_mean = torch.tensor(u_mean).repeat([n_players, 1]),
+                   expected_std = torch.tensor(u_std).repeat([n_players, 1]),
+                   expected_correlation=torch.eye(3)
+                   )
+
+    ## sample with a different batch size
+    v = s.sample(alternative_batch_size)
+    assert v.shape == torch.Size([alternative_batch_size, n_players, 1]), \
+        "failed to sample with nonstandard size."
+
+    ## sample on cpu
+    v = s.sample(device='cpu')
+    assert v.device.type == 'cpu', "sampling didn't respect device parameter."
+
+    ### test with valuation size 4.
+    valuation_size = 4
+    s = vs.UniformSymmetricIPVSampler(u_lo, u_hi, n_players, valuation_size, batch_size)
+    v = s.sample()
+    assert v.device.type == device, "Standard device should be cuda, if available!"
+
+    check_validity(v,
+                   expected_shape= torch.Size([batch_size, n_players, valuation_size]),
+                   expected_mean = torch.tensor(u_mean).repeat([n_players, valuation_size]),
+                   expected_std = torch.tensor(u_std).repeat([n_players, valuation_size]),
+                   )
+
+def test_gaussian_symmetric_ipv():
+    """Test the GaussianSymmetricIPVSampler."""
+
+    # test with valuation size 1.
+    s = vs.GaussianSymmetricIPVSampler(n_mean, n_std, n_players, 1, batch_size)
+
+
+    v = s.sample()
+    assert v.device.type == device, "Standard device should be cuda, if available!"
+
+    check_validity(v,
+                   expected_shape= torch.Size([batch_size, n_players, 1]),
+                   expected_mean = torch.tensor(n_mean).repeat([n_players, 1]),
+                   expected_std = torch.tensor(n_std).repeat([n_players, 1]),
+                   expected_correlation=torch.eye(3)
+                   )
+
+    ## sample with a different batch size
+    v = s.sample(alternative_batch_size)
+    assert v.shape == torch.Size([alternative_batch_size, n_players, 1]), \
+        "failed to sample with nonstandard size."
+
+    ## sample on cpu
+    v = s.sample(device='cpu')
+    assert v.device.type == 'cpu', "sampling didn't respect device parameter."
+
+    ### ensure valuation clipping at zero by using std=mean 
+    s = vs.GaussianSymmetricIPVSampler(n_mean, n_mean, n_players, 1, batch_size)
+    v = s.sample()
+    assert torch.all(v.ge(0)), "negative draws should be clipped to zero!"
+
+    ### test with valuation size 4.
+    valuation_size = 4
+    s = vs.GaussianSymmetricIPVSampler(n_mean, n_std, n_players, valuation_size, batch_size)
+    v = s.sample()
+    assert v.device.type == device, "Standard device should be cuda, if available!"
+
+    check_validity(v,
+                   expected_shape= torch.Size([batch_size, n_players, valuation_size]),
+                   expected_mean = torch.tensor(n_mean).repeat([n_players, valuation_size]),
+                   expected_std = torch.tensor(n_std).repeat([n_players, valuation_size]),
+                   )
--- bnelearn/valuation_sampler.py
+++ bnelearn/valuation_sampler.py
@@ -5,9 +5,10 @@ from typing import Tuple, List, Dict
 import torch
 from torch import distributions
 from torch.distributions import Distribution
+from torch.distributions.independent import Independent
 
 
-class ValuationObservationOracle(ABC):
+class ValuationObservationSampler(ABC):
     """Provides functionality to draw valuation and observation profiles."""
     n_players: int = None # The number of players in the valuation profile
     valuation_size: int = None # The dimensionality / length of a single valuation vector
@@ -15,7 +16,7 @@ class ValuationObservationOracle(ABC):
     default_batch_size: int = None # a default batch size 
 
     @abstractmethod
-    def draw_profiles(self, batch_size: int = None) -> Tuple(torch.Tensor, torch.Tensor):
+    def draw_profiles(self, batch_size: int = None) -> Tuple[torch.Tensor, torch.Tensor]:
         """Draws and returns a batch of valuation and observation profiles.
 
         Kwargs: 
@@ -31,7 +32,7 @@ class ValuationObservationOracle(ABC):
     def draw_conditional_profiles(self,
                                   conditioned_player: int,
                                   conditioned_observation: torch.Tensor,
-                                  batch_size: int) -> Tuple(torch.Tensor, torch.Tensor):
+                                  batch_size: int) -> Tuple[torch.Tensor, torch.Tensor]:
         """Draws and returns batches conditional valuation and corresponding observation profile.
         For each entry of `conditioned_observation`, `batch_size` samples will be drawn!
 
@@ -40,7 +41,7 @@ class ValuationObservationOracle(ABC):
         in that case, we need access to conditional observations of others, as well as
         conditional valuations of `conditioned_player`.
 
-        Note that here, we are returning full profiles instead (inlcuding 
+        Note that here, we are returning full profiles instead (including 
         `conditioned_player`'s observation and others' valuations.)
 
         Args:
@@ -64,14 +65,14 @@ class ValuationObservationOracle(ABC):
         """
 
 
-class ValuationObservationOracle(ABC):
+class ValuationSampler(ABC):
     """Provides functionality to draw valuation profiles."""
     n_players: int = None # The number of players in the valuation profile
     valuation_size: int = None # The dimensionality / length of a single valuation vector
     default_batch_size: int = None # a default batch size 
 
     @abstractmethod
-    def draw_profiles(self, batch_size: int = None) -> Tuple(torch.Tensor, torch.Tensor):
+    def sample(self, batch_size: int = None, device = None) -> torch.Tensor:
         """Draws and returns a batch of valuation profiles.
 
         Kwargs: 
@@ -83,27 +84,78 @@ class ValuationObservationOracle(ABC):
         """
 
 
-class SymmetricIPVOracle(ValuationObservationOracle): #TODO: change name to express that this is symmetric also along items, not just players?
+class SymmetricIPVSampler(ValuationSampler): #TODO: change name to express that this is symmetric also along items, not just players?
     """A Valuation Oracle that draws valuations independently and symmetrically
     for all players and each entry of their valuation vector according to a specified
     distribution.
+
+    This base class works with all torch.distributions but requires sampling on
+    cpu then moving to the device. When using cuda, use the faster, 
+    distribution-specific subclasses instead where provided.
     """
     def __init__(self, distribution: Distribution, 
                  n_players: int, valuation_size: int = 1,
-                 default_batch_size: int = 1
+                 default_batch_size: int = 1, default_device = None
                 ):
         """
         Args:
             distribution: a single-dimensional torch.distributions.Distribution.
             n_players: the number of players
             valuation_size: the length of each valuation vector
-            default_batch_size: the default batch size for sampling from this instance.
+            default_batch_size: the default batch size for sampling from this instance
+            default_device: the default device to draw valuations. If none given,
+                uses 'cuda' if available, 'cpu' otherwise
         """
         self.n_players = n_players
         self.valuation_size = valuation_size
         self.default_batch_size = default_batch_size
-        self.distribution = distribution
+        self.default_device = (default_device or 'cuda') if torch.cuda.is_available() else 'cpu'
+        self.base_distribution = distribution
+        self.distribution = self.base_distribution.expand([n_players, valuation_size])
 
-        
+    def sample(self, batch_size: int = None, device=None) -> torch.Tensor:
+        batch_size = batch_size or self.default_batch_size
+        device = device or self.default_device
 
+        return self.distribution.sample([batch_size]).to(device)
+
+class UniformSymmetricIPVSampler(SymmetricIPVSampler):
+    """An IPV sampler with symmetric Uniform priors."""
+    def __init__(self, lo, hi,
+                 n_players, valuation_size,
+                 default_batch_size, default_device = None):
+        distribution = torch.distributions.uniform.Uniform(low=lo, high=hi)
+        super().__init__(distribution,
+                         n_players, valuation_size,
+                         default_batch_size, default_device)
+
+    def sample(self, batch_size=None, device = None) -> torch.Tensor:
+        batch_size = batch_size or self.default_batch_size
+        device = device or self.default_device
+
+        # create an empty tensor on the output device, then sample in-place
+        return torch.empty(
+            [batch_size, self.n_players, self.valuation_size],
+            device=device).uniform_(self.base_distribution.low, self.base_distribution.high)
+
+
+class GaussianSymmetricIPVSampler(SymmetricIPVSampler):
+    """An IPV sampler with symmetric Gaussian priors."""
+    def __init__(self, mean, stddev,
+                 n_players, valuation_size,
+                 default_batch_size, default_device = None):
+        distribution = torch.distributions.normal.Normal(loc=mean, scale=stddev)
+        super().__init__(distribution,
+                         n_players, valuation_size,
+                         default_batch_size, default_device)
+
+    def sample(self, batch_size = None, device = None) -> torch.Tensor:
+        batch_size = batch_size or self.default_batch_size
+        device = device or self.default_device
+
+        # create empty tensor, sample in-place, clip
+        return torch.empty([batch_size, self.n_players, self.valuation_size],
+                           device=device) \
+            .normal_(self.base_distribution.loc, self.base_distribution.scale) \
+            .relu_()
 
