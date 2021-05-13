92d3f143f5bd467ffece259faf2bb4ed3564435c
kohring <nils.kohring@in.tum.de>
2021-05-13 13:21:04 +0200

    updated run script: common values

--- scripts/run_interdependent.py
+++ scripts/run_interdependent.py
@@ -21,59 +21,73 @@ if __name__ == '__main__':
 
     # User parameters
     specific_gpu = 7
-    n_runs = 1
-    n_epochs = 2000
+    n_runs = 10
 
     # model_sharing = True
     pretrain_iters = 500
 
     batch_size = 2**17
     eval_batch_size = 2**22
-    util_loss_frequency = 2000
+    util_loss_frequency = 1000
     util_loss_batch_size = 2**12
     util_loss_grid_size = 2**10
     stopping_criterion_frequency = 100000  # don't use
 
 
-    # def run(experiment_config, experiment_class): # pylint: disable=redefined-outer-name
-    #     """Run a experiment class from config"""
-    #     experiment = experiment_class(experiment_config)
-    #     if experiment.known_bne:
-    #         experiment.logging.log_metrics = {
-    #             'opt': True,
-    #             'l2': True,
-    #             'util_loss': True
-    #         }
-    #     experiment.logging.log_metrics['efficiency'] = True
-    #     experiment.logging.log_metrics['revenue'] = True
+    def run(experiment_config, experiment_class): # pylint: disable=redefined-outer-name
+        """Run a experiment class from config"""
+        experiment = experiment_class(experiment_config)
+        if experiment.known_bne:
+            experiment.logging.log_metrics = {
+                'opt': True,
+                'l2': True,
+                'util_loss': True
+            }
+        experiment.logging.log_metrics['efficiency'] = True
+        experiment.logging.log_metrics['revenue'] = True
+        experiment.logging.best_response = True
 
-    #     experiment.logging.util_loss_batch_size = 2**12
-    #     experiment.logging.util_loss_grid_size = 2**10
-    #     experiment.logging.util_loss_frequency = n_epochs
-    #     experiment.logging.best_response = True
+        try:
+            experiment.run()
 
-    #     try:
-    #         experiment.run()
-
-    #     except Exception: # pylint: disable=broad-except
-    #         traceback.print_exc()
-    #         torch.cuda.empty_cache()
+        except Exception: # pylint: disable=broad-except
+            traceback.print_exc()
+            torch.cuda.empty_cache()
 
 
-    # # Mineral rights
-    # n_players = 3
-    # experiment_config, experiment_class = \
-    #     ConfigurationManager(experiment_type='mineral_rights') \
-    #         .get_config(
-    #             log_root_dir=log_root_dir,
-    #             n_runs=n_runs,
-    #             n_epochs=n_epochs,
-    #             specific_gpu=specific_gpu,
-    #             n_players=n_players,
-    #             correlation_groups=[list(range(n_players))],
-    #             # pretrain_iters=10
-    #         )
-    # run(experiment_config, experiment_class)
+    # Mineral rights
+    log_root_dir = os.path.join(
+        os.path.expanduser('~'), 'bnelearn', 'experiments', 'erratum'
+    )
+    for n_players, n_epochs in zip([3, 10], [1000, 2000]):
+        experiment_config, experiment_class = \
+            ConfigurationManager(
+                experiment_type='mineral_rights',
+                n_runs=n_runs,
+                n_epochs=n_epochs,
+            ) \
+            .set_learning(
+                pretrain_iters=pretrain_iters,
+                batch_size=batch_size,
+            ) \
+            .set_setting(
+                correlation_groups=[list(range(n_players))],
+                n_players=n_players,
+            ) \
+            .set_hardware(
+                specific_gpu=specific_gpu,
+            ) \
+            .set_logging(
+                log_root_dir=log_root_dir,
+                eval_batch_size=eval_batch_size,
+                util_loss_frequency=util_loss_frequency,
+                util_loss_batch_size=util_loss_batch_size,
+                util_loss_grid_size=util_loss_grid_size,
+                stopping_criterion_frequency=stopping_criterion_frequency, 
+            ) \
+            .get_config()
+
+        run(experiment_config, experiment_class)
 
     # # Affiliated observations
     # log_root_dir = os.path.join(
@@ -107,48 +121,48 @@ if __name__ == '__main__':
     # experiment.run()
 
 
-    # Run LLG nearest-vcg for different risks / correlations ################
-    log_root_dir = os.path.join(
-        os.path.expanduser('~'), 'bnelearn', 'experiments', 'regret_winner'
-    )
-    regrets = [0.0, 0.1, 0.2, 0.3, 0.4]
-    gammas = [0.0, 0.5, 1.0]  # list(i/10 for i in range(0, 11))
-    payment_rules = ['nearest_vcg', 'first_price']
-    corr_models = ['constant_weights', 'Bernoulli_weights']
-    for corr_model in corr_models:
-        for gamma in gammas:
-            for payment_rule in payment_rules:
-                for regret in regrets:
-                    experiment_config, experiment_class = \
-                        ConfigurationManager(
-                            experiment_type='llg',
-                            n_runs=n_runs,
-                            n_epochs=n_epochs) \
-                            .set_setting(
-                                payment_rule=payment_rule,
-                                gamma=gamma,
-                                correlation_types=corr_model,
-                                # loss_aversion = True,
-                                regret=regret
-                                ) \
-                            .set_learning(
-                                batch_size=batch_size,
-                                pretrain_iters=pretrain_iters,
-                                model_sharing=False,
-                                ) \
-                            .set_logging(
-                                log_root_dir=log_root_dir,
-                                util_loss_frequency=util_loss_frequency,
-                                util_loss_batch_size=util_loss_batch_size,
-                                util_loss_grid_size=util_loss_grid_size,
-                                eval_batch_size=eval_batch_size,
-                                stopping_criterion_frequency= \
-                                    stopping_criterion_frequency,
-                                ) \
-                            .set_hardware(specific_gpu=specific_gpu) \
-                            .get_config()
-                    experiment = experiment_class(experiment_config)
-                    experiment.run()
+    # # Run LLG nearest-vcg for different risks / correlations ################
+    # log_root_dir = os.path.join(
+    #     os.path.expanduser('~'), 'bnelearn', 'experiments', 'regret_winner'
+    # )
+    # regrets = [0.0, 0.1, 0.2, 0.3, 0.4]
+    # gammas = [0.0, 0.5, 1.0]  # list(i/10 for i in range(0, 11))
+    # payment_rules = ['nearest_vcg', 'first_price']
+    # corr_models = ['constant_weights', 'Bernoulli_weights']
+    # for corr_model in corr_models:
+    #     for gamma in gammas:
+    #         for payment_rule in payment_rules:
+    #             for regret in regrets:
+    #                 experiment_config, experiment_class = \
+    #                     ConfigurationManager(
+    #                         experiment_type='llg',
+    #                         n_runs=n_runs,
+    #                         n_epochs=n_epochs) \
+    #                         .set_setting(
+    #                             payment_rule=payment_rule,
+    #                             gamma=gamma,
+    #                             correlation_types=corr_model,
+    #                             # loss_aversion = True,
+    #                             regret=regret
+    #                             ) \
+    #                         .set_learning(
+    #                             batch_size=batch_size,
+    #                             pretrain_iters=pretrain_iters,
+    #                             model_sharing=False,
+    #                             ) \
+    #                         .set_logging(
+    #                             log_root_dir=log_root_dir,
+    #                             util_loss_frequency=util_loss_frequency,
+    #                             util_loss_batch_size=util_loss_batch_size,
+    #                             util_loss_grid_size=util_loss_grid_size,
+    #                             eval_batch_size=eval_batch_size,
+    #                             stopping_criterion_frequency= \
+    #                                 stopping_criterion_frequency,
+    #                             ) \
+    #                         .set_hardware(specific_gpu=specific_gpu) \
+    #                         .get_config()
+    #                 experiment = experiment_class(experiment_config)
+    #                 experiment.run()
 
 
 
