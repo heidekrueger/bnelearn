ade6b62b0e25a1b275caafe5a5cf4e5799bf9670
Gleb <glebk98@gmail.com>
2021-05-22 14:41:58 +0200

    WIP optimizable experiment runs

--- bnelearn/bopt/main.py
+++ bnelearn/bopt/main.py
@@ -1,4 +1,13 @@
-from bayes_opt import BayesianOptimization
+import os
+import sys
+from BayesianOptimization.bayes_opt import BayesianOptimization
+
+# put bnelearn imports after this.
+# pylint: disable=wrong-import-position
+sys.path.append(os.path.realpath('.'))
+sys.path.append(os.path.join(os.path.expanduser('~'), 'bnelearn'))
+from bnelearn.experiment.configurations import ExperimentConfig # pylint: disable=import-error
+from bnelearn.experiment.configuration_manager import ConfigurationManager  # pylint: disable=import-error
 
 def black_box_function(x, y):
     """Function with unknown internals we wish to maximize.
@@ -7,12 +16,17 @@ def black_box_function(x, y):
     purposes think of the internals of this function, i.e.: the process
     which generates its output values, as unknown.
     """
+    #experiment_config, experiment_class = ConfigurationManager(experiment_type='single_item_uniform_symmetric', n_runs=2, n_epochs=3) \
+    #    .get_config()
+    #experiment = experiment_class(experiment_config)                                                             
+
     return -x ** 2 - (y - 1) ** 2 + 1
 
 
-
+#print(black_box_function(2,1))
 # Bounded region of parameter space
-pbounds = {'x': (2, 4), 'y': (-3, 3)}
+pbounds = {'x': (-100, 100), 'y': (-100, 100)}
+pbounds = {'x': (0, 4), 'y': (-3, 3)}
 
 optimizer = BayesianOptimization(
     f=black_box_function,
@@ -21,14 +35,9 @@ optimizer = BayesianOptimization(
     random_state=1,
 )
 
-optimizer.maximize(
-    init_points=2,
-    n_iter=20,
-)
+optimizer.maximize(init_points=5, n_iter=10)
 
-
+#for i, res in enumerate(optimizer.res):
+#    print("Iteration {}: \n\t{}".format(i, res))
 
 print(optimizer.max)
-
-for i, res in enumerate(optimizer.res):
-    print("Iteration {}: \n\t{}".format(i, res))
--- bnelearn/bopt/optimizable_experiment.py
+++ bnelearn/bopt/optimizable_experiment.py
@@ -1,76 +1,93 @@
 import os
 import sys
 import numpy as np
-from bayes_opt import BayesianOptimization
-
+from BayesianOptimization.bayes_opt import BayesianOptimization
 
 # put bnelearn imports after this.
 # pylint: disable=wrong-import-position
 sys.path.append(os.path.realpath('.'))
 sys.path.append(os.path.join(os.path.expanduser('~'), 'bnelearn'))
-
 from bnelearn.experiment.configurations import ExperimentConfig # pylint: disable=import-error
 from bnelearn.experiment.configuration_manager import ConfigurationManager  # pylint: disable=import-error
 
 class OptimizableExperiment:
     """
     Optimize hp's using bayes_opt    
-    """
-    
-    def __init__(self, experiment_class, experiment_config: ExperimentConfig, hp_bounds: dict, n_runs_budget = 5, save_log: bool = True):
+    """   
+    def __init__(self, experiment_class, experiment_config: ExperimentConfig, hp_bounds: dict, n_runs_budget = 5, save_log: bool = True, 
+                metric_name: str = 'epsilon_relative'):
         """
-        Format for the bounds: {'x': (2, 4), 'y': (-3, 3)}
+        Format for the hp_bounds: {'x': (2, 4), 'y': (-3, 3)}, where x and y are some hp's
+        n_runs_budget is the number of effective hp configurations to check, to get the full number of runs, 
+        multiply by the number of seeds per run
         """
+        print(type(experiment_class))
         self.experiment_class = experiment_class
         self.experiment_config = experiment_config        
         self.hp_bounds = hp_bounds
         self.n_runs_budget = n_runs_budget
+        self.metric_name = metric_name
     
     # verbose = 1 prints only when a maximum is observed, verbose = 0 is silent
-    def optimize(self):
-        optimizer = BayesianOptimization(f=self._optimizable_function, pbounds=self.hp_bounds, verbose=2, random_state=1)
-        optimizer.maximize(init_points=2, n_iter=3,)
-
-        print(optimizer.max)
+    def optimize(self, init_points: int = 3, minimize: bool = True):
+        optimizer = BayesianOptimization(f=self._get_optimizable_function(minimize=minimize), pbounds=self.hp_bounds, verbose=2, random_state=1)        
+        optimizer.maximize(init_points=init_points, n_iter=self.n_runs_budget)            
 
         for i, res in enumerate(optimizer.res):
             print("Iteration {}: \n\t{}".format(i, res))
-
-    def _optimizable_function(self, lr=1e-3) -> float:
-        self.experiment_config.learning.optimizer_hyperparams['lr'] = lr
         
-        experiment = self.experiment_class(self.experiment_config)
-        cur_res = experiment.run()
-        specific_metric = self._get_specific_metric(result=cur_res, metric_name='epsilon_relative')
-        #save metric/result
-        return specific_metric
+        print(optimizer.max)
+
+
+    def _get_optimizable_function(self, minimize: bool):
+        #If we want hp's to be dynamically determined from the given bounds, we would need to create this method (its signature) dynamically
+        def optimizable_function(lr: float = 1e-3) -> float:            
+            self.experiment_config.learning.optimizer_hyperparams['lr'] = lr        
+            experiment = self.experiment_class(self.experiment_config)
+            res = experiment.run()
+            res = res.to_numpy()
+            specific_metric = self._get_specific_metric(result=res)
+            #save metric/result
+            if minimize == True:
+                return -specific_metric
+            else:
+                return specific_metric
+
+        return optimizable_function
+
     
-    #TODO specify result type
-    def _get_specific_metric(self, result, metric_name: str = 'epsilon_relative'):
+    def _get_specific_metric(self, result: np.ndarray) -> float:
+        """
+        Takes experiment result and returns an avereged across all seeds specified metric.
+        Possible metric names include: utilities, update_norm, utility_vs_bne, epsilon_relative, epsilon_absolute,
+        L_2, L_inf, util_loss_ex_ante, util_loss_ex_interim, estimated_relative_ex_ante_util_loss
+        """
         eps_rel = []
         for metric_entry in result:
-            if metric_entry[2] == 'eval/epsilon_relative':                    
-                print(metric_entry)
+            if metric_entry[2] == 'eval/{}'.format(self.metric_name):                    
+                #print(metric_entry)
                 eps_rel.append(metric_entry[4])                    
+                print(metric_entry)
 
-            np.array(eps_rel)
-            print(np.average(eps_rel))
+        np.array(eps_rel)
+        averaged_metric = np.average(eps_rel)
+        print('Averaged:' + str(averaged_metric))
 
-        return 0
+        return averaged_metric
 
 try:
     log_root_dir = os.path.join(os.path.expanduser('~'), 'bnelearn', 'experiments')
 
     #n_runs here means # of seeds
-    experiment_config, experiment_class = ConfigurationManager(experiment_type='single_item_uniform_symmetric', n_runs=2,
-                                                                n_epochs=3) \
+    experiment_config, experiment_class = ConfigurationManager(experiment_type='single_item_uniform_symmetric', n_runs=3, n_epochs=500) \
             .set_setting(risk=1.1)\
             .set_logging(log_root_dir=log_root_dir, save_tb_events_to_csv_detailed=True)\
             .set_learning(pretrain_iters=5) \
             .set_logging(eval_batch_size=2**22).set_hardware(specific_gpu=7).get_config()
 
-    experiment = OptimizableExperiment(experiment_class=experiment_class, experiment_config=experiment_config, hp_bounds={}, n_runs_budget=20)
-    experiment._optimizable_function()
+    experiment = OptimizableExperiment(experiment_class=experiment_class, experiment_config=experiment_config, hp_bounds={'lr': (0.0001, 0.01)},
+                                        n_runs_budget=20)
+    experiment.optimize()
 
 except KeyboardInterrupt:
     print('\nKeyboardInterrupt: released memory after interruption')
