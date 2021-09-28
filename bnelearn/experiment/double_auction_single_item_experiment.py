e82328afd55053adcd551cf0990263886a8d49fc
ge69hoj <fabian.pieroth@tum.de>
2021-09-28 15:02:54 +0200

    Delete additional plotting of current pretrained BNE

--- bnelearn/experiment/double_auction_single_item_experiment.py
+++ bnelearn/experiment/double_auction_single_item_experiment.py
@@ -198,7 +198,7 @@ class DoubleAuctionUniformSymmetricPriorSingleItemExperiment(DoubleAuctionSymmet
             self._optimal_bid = [bne_bilateral_bargaining_uniform_linear(
                 self.config, self.u_lo, self.u_hi)]
             if self.k == 0.5 and self.u_lo == 0 and self.u_hi == 1:
-                self._optimal_bid += bne_bilateral_bargaining_uniform_symmetric(self.config, [self.config.setting.pretrain_transform, 0.25, 0.45])
+                self._optimal_bid += bne_bilateral_bargaining_uniform_symmetric(self.config)
             return True
 
         return super()._check_and_set_known_bne()
