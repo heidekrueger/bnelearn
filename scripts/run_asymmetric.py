e64846747712497899f46daa6a6f09d03a61f780
kohring <nils.kohring@in.tum.de>
2021-10-15 18:29:54 +0200

    asymmetric run script

---
+++ scripts/run_asymmetric.py
@@ -0,0 +1,181 @@
+"""
+Runs predefined experiments with individual parameters
+fire.Fire() asks you to decide for one of the experiments defined above
+by writing its name and define the required (and optional) parameters
+e.g.:
+    experiment.py single_item_uniform_symmetric 1 20 [2,3] 'first_price'
+
+alternatively instead of fire.Fire() use, e.g.:
+    single_item_uniform_symmetric(1,20,[2,3],'first_price')
+
+"""
+import os
+import sys
+
+import torch
+
+# put bnelearn imports after this.
+# pylint: disable=wrong-import-position
+sys.path.append(os.path.realpath('.'))
+sys.path.append(os.path.join(os.path.expanduser('~'), 'bnelearn'))
+
+from bnelearn.experiment.configuration_manager import ConfigurationManager  # pylint: disable=import-error
+
+
+if __name__ == '__main__':
+
+    # Path is user-specific
+    log_root_dir = os.path.join(
+        os.path.expanduser('~'), 'bnelearn', 'experiments', 'asymmmetric'
+        )
+
+
+    # Single-item asymmetric experiments ######################################
+    # experiment_types = ['single_item_asymmetric_uniform_overlapping',
+    #                     'single_item_asymmetric_uniform_disjunct']
+    # for experiment_type in experiment_types:
+    #     experiment_config, experiment_class = \
+    #         ConfigurationManager(
+    #             experiment_type=experiment_type,
+    #             n_runs=1, n_epochs=2000,
+    #             ) \
+    #         .set_learning(
+    #             pretrain_iters=500,
+    #             # learner_hyperparams={
+    #             #     'population_size': 64,
+    #             #     'sigma': .1,
+    #             #     'scale_sigma_by_model_size': True
+    #             # },
+    #             ) \
+    #         .set_logging(
+    #             eval_batch_size=2**22,
+    #             util_loss_batch_size=2**6,
+    #             util_loss_grid_size=2**10,
+    #             util_loss_frequency=50,
+    #             plot_frequency=50,
+    #             log_root_dir=log_root_dir,
+    #             best_response=True
+    #             ) \
+    #         .set_hardware(
+    #             specific_gpu=2
+    #         ) \
+    #         .get_config()
+    # experiment = experiment_class(experiment_config)
+    # experiment.run()
+    # torch.cuda.empty_cache()
+
+
+    # Asymmetric LLG experiment ###############################################
+    # experiment_config, experiment_class = \
+    #     ConfigurationManager(
+    #         experiment_type='llg_full',
+    #         n_runs=10, n_epochs=2000,
+    #         ) \
+    #     .set_setting(
+    #         payment_rule='mrcs_favored',
+    #         ) \
+    #     .set_learning(
+    #         batch_size=2**15,
+    #         ) \
+    #     .set_logging(
+    #         eval_batch_size=2**15,
+    #         util_loss_batch_size=2**12,
+    #         util_loss_grid_size=2**10,
+    #         util_loss_frequency=100,
+    #         best_response=True,
+    #         plot_frequency=100,
+    #         cache_eval_actions=True,
+    #         log_root_dir=log_root_dir,
+    #         ) \
+    #     .set_hardware(
+    #         specific_gpu=7
+    #         ) \
+    #     .get_config()
+    # experiment = experiment_class(experiment_config)
+    # experiment.run()
+    # torch.cuda.empty_cache()
+
+
+    # Split-award auction #####################################################
+    # experiment_config, experiment_class = \
+    #     ConfigurationManager(
+    #         experiment_type='splitaward',
+    #         n_runs=10, n_epochs=2000
+    #             ) \
+    #         .set_setting(
+    #             ) \
+    #         .set_learning(
+    #             ) \
+    #         .set_logging(
+    #             eval_batch_size=2**15,
+    #             util_loss_batch_size=2**12,
+    #             util_loss_grid_size=2**10,
+    #             util_loss_frequency=2000,
+    #             best_response=True,
+    #             plot_frequency=500,
+    #             cache_eval_actions=True,
+    #             log_root_dir=log_root_dir,
+    #             ) \
+    #         .set_hardware(
+    #             specific_gpu=7
+    #             ) \
+    #         .get_config()
+    # experiment = experiment_class(experiment_config)
+    # experiment.run()
+    # torch.cuda.empty_cache()
+
+
+    ### COMBINATRORIAL EXPERIMENTS ###
+    # experiment_config, experiment_class = \
+    #     ConfigurationManager(
+    #         experiment_type='llllgg', n_runs=1, n_epochs=500
+    #         ) \
+    #         .set_setting(
+    #             core_solver='mpc',
+    #             payment_rule='nearest_vcg'
+    #             ) \
+    #         .set_learning(
+    #             pretrain_iters=50,
+    #             batch_size=2**8,
+    #             learner_hyperparams={
+    #                 'population_size': 16,
+    #                 'sigma': 1.,
+    #                 'scale_sigma_by_model_size': True
+    #                 },
+    #             ) \
+    #         .set_logging(
+    #             log_root_dir=log_root_dir,
+    #             log_metrics={'util_loss': True},
+    #             util_loss_batch_size=2**7,
+    #             util_loss_grid_size=2**6,
+    #             util_loss_frequency=10,
+    #             plot_frequency=10
+    #             ) \
+    #         .set_hardware(
+    #             specific_gpu=7
+    #             ) \
+    #         .get_config()
+    # experiment = experiment_class(experiment_config)
+    # experiment.run()
+    # torch.cuda.empty_cache()
+
+
+    # Combinatoriral auction with item bidding ################################
+    # experiment_config, experiment_class = \
+    #     ConfigurationManager(
+    #         'caib', n_runs=10,
+    #         n_epochs=2000
+    #         ) \
+    #     .set_setting(
+    #         n_players=2,
+    #         n_items=3,
+    #         exp_params={
+    #             'n_collections': 1,
+    # #           'one_player_w_unit_demand': True
+    #              },
+    # #       exp_params={'submodular_factor': .9},\
+    #         ) \
+    #     .get_config()
+    # experiment = experiment_class(experiment_config)
+    # experiment.run()
+    # torch.cuda.empty_cache()
