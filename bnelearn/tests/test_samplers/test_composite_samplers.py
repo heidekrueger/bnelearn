02e54b2337a8d97e20b2aa74657b64d3f9a48af6
Nils <nils.kohring@in.tum.de>
2022-01-11 16:10:25 +0100

    sampler test fix

--- bnelearn/tests/test_samplers/test_composite_samplers.py
+++ bnelearn/tests/test_samplers/test_composite_samplers.py
@@ -109,7 +109,8 @@ def test_local_global_samplers(setting, method, gamma):
     ASSERTION_ERROR_UNEXPECTED_GRID = "unexpected grid for local bidder."
 
     n_points = 2**valuation_size
-    grid_local = s.generate_valuation_grid(local_indices[0], n_points)
+    grid_local = s.generate_valuation_grid(player_position=local_indices[0],
+                                           minimum_number_of_points=n_points)
 
     assert torch.equal(grid_local.min(dim=0).values,
                        torch.tensor([local_min]*valuation_size, device=device)), \
@@ -118,7 +119,8 @@ def test_local_global_samplers(setting, method, gamma):
                        torch.tensor([local_max]*valuation_size, device=device)), \
                            ASSERTION_ERROR_UNEXPECTED_GRID
 
-    grid_global = s.generate_valuation_grid(global_indices[0], n_points)
+    grid_global = s.generate_valuation_grid(player_position=global_indices[0],
+                                            minimum_number_of_points=n_points)
 
     assert torch.equal(grid_global.min(dim=0).values,
                        torch.tensor([global_min]*valuation_size, device=device)), \
