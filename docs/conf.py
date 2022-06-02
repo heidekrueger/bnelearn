0bc38b225754ec699d088be1fbdf152e457ba5b5
Nils <nils.kohring@in.tum.de>
2022-06-02 16:23:14 +0200

    frontend look changes

--- docs/conf.py
+++ docs/conf.py
@@ -60,8 +60,11 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_logo = "bnelearn-logo.png"
+html_logo = "bnelearn-gray.png"
 
 
 #display private members 
 #autodoc_default_options = {     "members": True,     "undoc-members": True, "private-members": True  }
+
+def setup(app):
+    app.add_css_file('css/modify.css')
