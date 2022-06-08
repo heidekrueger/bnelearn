250104a85d1eb71323a793668381003d10d30918
Stefan Heidekrüger <stefan.heidekrueger@tum.de>
2022-06-08 11:41:43 +0200

    changed relative path in readthedocs yaml

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../bnelearn'))
 
 
 # -- Project information -----------------------------------------------------
@@ -25,14 +25,14 @@ author = 'Chair for Decision Sciences and Systems, TUM'
 release = '1.0.0'
 
 
-def run_apidoc(_):
-	from sphinx.apidoc import main
-	import os
-	import sys
-	sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-	cur_dir = os.path.abspath(os.path.dirname(__file__))
-	module = os.path.join(cur_dir,"..",".")
-	main(['-e', '-o', cur_dir, module, '--force'])
+# def run_apidoc(_):
+# 	from sphinx.apidoc import main
+# 	import os
+# 	import sys
+# 	sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+# 	cur_dir = os.path.abspath(os.path.dirname(__file__))
+# 	module = os.path.join(cur_dir,"..",".")
+# 	main(['-e', '-o', cur_dir, module, '--force'])
 
 # -- General configuration ---------------------------------------------------
 
@@ -75,5 +75,5 @@ html_logo = "bnelearn-gray.png"
 #autodoc_default_options = {     "members": True,     "undoc-members": True, "private-members": True  }
 
 def setup(app):
-    app.connect('builder-inited', run_apidoc)
+    #app.connect('builder-inited', run_apidoc)
     app.add_css_file('css/modify.css')
