2c4dab87784ee883847b50cf7ecc4254f245245c
Stefan Heidekrüger <stefan.heidekrueger@tum.de>
2022-06-08 12:12:10 +0200

    another try at autodoc...

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,9 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../bnelearn/'))
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('./bnelearn/'))
+
+
 
 # -- Project information -----------------------------------------------------
 
